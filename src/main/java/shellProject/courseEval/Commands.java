a7b73c9f7b1da2b7e323d567947dc63cb6807674
Shuai Huang <shhuang9532@gmail.com>
2020-06-16 16:09:08 -0700

    start

---
+++ src/main/java/shellProject/courseEval/Commands.java
@@ -0,0 +1,13 @@
+package shellProject.courseEval;
+
+import org.springframework.shell.standard.ShellComponent;
+import org.springframework.shell.standard.ShellMethod;
+
+@ShellComponent
+public class Commands {
+
+    @ShellMethod("Add two integers together.")
+    public int add(int a, int b) {
+        return a + b;
+    }
+}
